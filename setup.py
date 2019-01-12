0396673e8868eaf0a4166cc35e611d7e2cb4bf92
Sam Klingner <stk1@me.com>
2019-01-12 17:28:23 +0100

    Trying out fixing the websocket-client version

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='pyhomee',
       version='0.0.2',
       description='Access Homee Websocket API',
-      url='http://github.com/twendt/pyhomee',
+      url='http://github.com/Stklingner/pyhomee',
       author='Timo Wendt',
       license='MIT',
-      install_requires=['websocket-client>=0.44.0', 'requests'],
+      install_requires=['websocket-client==0.44.0', 'requests'],
       packages=find_packages(),
       zip_safe=True)
